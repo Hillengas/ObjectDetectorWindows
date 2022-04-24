5ec111762697682211259328054593a54d01b1da
Hillengas <alexander_gebhard@web.de>
2022-04-24 14:20:53 +0200

    objectDetector for clothes created

--- ObjectDetectorUI/ObjectDetectorUI/Form1.Designer.cs
+++ ObjectDetectorUI/ObjectDetectorUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ObjectDetectorUI
 {
-    partial class Form1
+    partial class FormPictureRecognizer
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPictureRecognizer));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnPictureChooser = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.BtnDoML = new System.Windows.Forms.Button();
+            this.TxtBoxResultML = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(620, 295);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnPictureChooser
+            // 
+            this.BtnPictureChooser.Location = new System.Drawing.Point(583, 332);
+            this.BtnPictureChooser.Name = "BtnPictureChooser";
+            this.BtnPictureChooser.Size = new System.Drawing.Size(119, 23);
+            this.BtnPictureChooser.TabIndex = 1;
+            this.BtnPictureChooser.Text = "Bild auswählen";
+            this.BtnPictureChooser.UseVisualStyleBackColor = true;
+            this.BtnPictureChooser.Click += new System.EventHandler(this.BtnPictureChooser_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // BtnDoML
+            // 
+            this.BtnDoML.Location = new System.Drawing.Point(502, 332);
+            this.BtnDoML.Name = "BtnDoML";
+            this.BtnDoML.Size = new System.Drawing.Size(75, 23);
+            this.BtnDoML.TabIndex = 2;
+            this.BtnDoML.Text = "Analysieren";
+            this.BtnDoML.UseVisualStyleBackColor = true;
+            this.BtnDoML.Click += new System.EventHandler(this.BtnDoML_Click);
+            // 
+            // TxtBoxResultML
+            // 
+            this.TxtBoxResultML.Location = new System.Drawing.Point(82, 335);
+            this.TxtBoxResultML.Name = "TxtBoxResultML";
+            this.TxtBoxResultML.ReadOnly = true;
+            this.TxtBoxResultML.Size = new System.Drawing.Size(100, 20);
+            this.TxtBoxResultML.TabIndex = 3;
+            // 
+            // FormPictureRecognizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.TxtBoxResultML);
+            this.Controls.Add(this.BtnDoML);
+            this.Controls.Add(this.BtnPictureChooser);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormPictureRecognizer";
+            this.Text = "Kleindungserkennung";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnPictureChooser;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button BtnDoML;
+        private System.Windows.Forms.TextBox TxtBoxResultML;
     }
 }
 
--- ObjectDetectorUI/ObjectDetectorUI/Form1.cs
+++ ObjectDetectorUI/ObjectDetectorUI/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,101 @@ using System.Windows.Forms;
 
 namespace ObjectDetectorUI
 {
-    public partial class Form1 : Form
+    public partial class FormPictureRecognizer : Form
     {
-        public Form1()
+        private string _imagePath = "";
+
+        public FormPictureRecognizer()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void BtnPictureChooser_Click(object sender, EventArgs e)
+        {
+            // open file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // display image in picture box
+                pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                _imagePath = openFileDialog.FileName;
+            }
+        }
+
+        private static string RunPythonFileML(string path)
+        {
+            var start = new ProcessStartInfo
+            {
+                FileName = @"C:\Users\Alex\AppData\Local\Programs\Python\Python36\python.exe",
+                Arguments = @"C:\Users\Alex\Desktop\load_tf_model.py",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+            // add _imagePath to the arguments
+            start.Arguments += " " + path;
+
+            var result = "";
+            
+            using (var process = Process.Start(start))
+            {
+                using (var reader = process.StandardOutput)
+                {
+                    result = reader.ReadToEnd();
+                    Console.Write(result);
+                }
+
+                using (var reader = process.StandardError)
+                {
+                    var resultError = reader.ReadToEnd();
+                    Console.Write(resultError);
+                }
+            }
+
+            return result;
+        }
+
+        private void BtnDoML_Click(object sender, EventArgs e)
+        {
+            var result = "";
+            if (_imagePath != "") result = RunPythonFileML(_imagePath);
+            result = result.Replace("\r\n", string.Empty);
+            
+            // cast result to PictureClasses
+            var pictureClasses = (PictureClasses)Enum.Parse(typeof(PictureClasses), result);
+            
+            // display result
+            TxtBoxResultML.Text = pictureClasses.ToString();
+
+        }
+
+        // create enum for picture classes of fashion
+        public enum PictureClasses
+        {
+            [Description("T-shirt")]
+            Tshirt,
+            [Description("Trouser")]
+            Trouser,
+            [Description("Pullover")]
+            Pullover,
+            [Description("Dress")]
+            Dress,
+            [Description("Coat")]
+            Coat,
+            [Description("Sandal")]
+            Sandal,
+            [Description("Shirt")]
+            Shirt,
+            [Description("Sneaker")]
+            Sneaker,
+            [Description("Bag")]
+            Bag,
+            [Description("Ankle boot")]
+            AnkleBoot
         }
     }
 }
